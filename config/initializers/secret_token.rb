7e8376b687a008083a5a3ee5aa9f250def6169e2
Edson Lima <edsonlima@edson-notebook.(none)>
2013-08-09 08:23:04 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrhindicacao::Application.config.secret_token = 'ed423a1d5bf5969938fe02e7984830f7cfb1283846825e2031b0cf32b3a4778b4a3cff2cacf18b70628b3373b6418c2e82020389a90d6ad8dc8b0baeab9e0382'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Mrhindicacao::Application.routes.draw do
+  
+  devise_for :users
+
+  root :to => 'application#index'
+
+  
+end

27092ea8b64ac41a705601437f31a7c1c989abf7
Edson Lima <edsonlima@edson-notebook.(none)>
2013-09-08 09:56:36 -0300

    valições em indicação

--- app/models/indicacao.rb
+++ app/models/indicacao.rb
@@ -1,6 +1,12 @@
+#coding: utf-8
 class Indicacao < ActiveRecord::Base
+
   attr_accessible :cliente_id, :data, :detalhes, :servico_id, :usuario_id
   belongs_to :user
   belongs_to :cliente
   belongs_to :servico
+
+  validates :user,    :presence => {:message => "Usuário não pode estar em branco"}
+  validates :cliente, :presence => {:message => "NÃO PODE ESTAR EM BRANCO"}
+  validates :servico, :presence => {:message => "NÃO PODE ESTAR EM BRANCO"}
 end

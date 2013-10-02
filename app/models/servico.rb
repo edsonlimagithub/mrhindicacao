ec38adfeb2c33d98d6a06eadc65cd3c68ef96532
Edson Lima <edsonlima@edson-notebook.(none)>
2013-10-01 23:11:34 -0300

    adicionado a listagem de serviços

--- app/models/servico.rb
+++ app/models/servico.rb
@@ -1,6 +1,6 @@
 #coding: utf-8
 class Servico < ActiveRecord::Base
-  attr_accessible :descricao, :porcentagem, :email, :detalhes
+  attr_accessible :descricao, :porcentagem, :email, :detalhes, :comissao_valor
 
    validates :email,    :presence => {:message => "não pode ficar em branco."}
 

330dbe9dd767ecfeccc346f4e78d81aa491c7113
Edson Lima <edsonlima@edson-notebook.(none)>
2014-04-28 15:28:56 -0300

    mudança de sequincia de envio de email

--- app/controllers/indicacaos_controller.rb
+++ app/controllers/indicacaos_controller.rb
@@ -114,8 +114,9 @@ class IndicacaosController < ApplicationController
   def enviar_email
     params[:indicacoes].each do |i, index|
       indicacao = Indicacao.find(i)
-      Email.indicacao(indicacao.servico.email, indicacao).deliver
       Email.indicacao('visitas@mrhgestao.com.br', indicacao).deliver
+      Email.indicacao(indicacao.servico.email, indicacao).deliver
+      
       indicacao.email_enviado = indicacao.email_enviado.to_i + 1
       indicacao.save
     end
